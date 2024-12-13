298bddc888e1d4409176b5ef3a593d9e212a63bd
Silvio Lima <silviolima07@gmail.com>
2024-12-13 16:38:24 -0300

    Update my_agents.py

--- my_agents.py
+++ my_agents.py
@@ -1,15 +1,18 @@
 from crewai import Agent
 import streamlit as st
 from textwrap import dedent
+from config_llm import MyLLM
 
-def criar_agente(modelo) -> Agent:
+llm = MyLLM.GROQ_LLAMA
+
+def criar_agente() -> Agent:
     #st.write("Agente criado")
     return Agent(
          role="Nutricionista",
          goal="Identificar se alimentos descritos no texto são saudáveis ou não.",
          allow_delegation=False,  # Define se o agente pode delegar tarefas
          tools=[], 
-         llm=modelo,
+         llm=llm,
          backstory=dedent("""
               Você é um especialista em nutrição com experiência em identificar comidas saudáveis ou não."""
      )
