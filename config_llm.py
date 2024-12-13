0505a6e3f2889afc64d92fea3cbea2010bd58498
Silvio Lima <silviolima07@gmail.com>
2024-12-13 14:02:09 -0300

    Update config_llm.py

--- config_llm.py
+++ config_llm.py
@@ -1,14 +1,2 @@
-from dotenv import load_dotenv
-import os
-import groq
-
-# Carregar variáveis de ambiente
-#load_dotenv()
-
-# Obter a chave da API GROQ
-#GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-
-llava_groq = "llava-v1.5-7b-4096-preview" 
-
 llama_groq = "llama-3.2-90b-vision-preview" 
       
