87b5cae7730be9c9dee01b1a16903a4369a0376f
Silvio Lima <silviolima07@gmail.com>
2024-12-13 19:32:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -87,6 +87,8 @@ client = Groq(api_key=GROQ_API_KEY)
 llama = MyLLM.GROQ_LLAMA.model
 #llama = llama_groq
 
+st.markdown("### LLM: "+llama)
+
 html_page_title = """
 <div style="background-color:black;padding=60px">
         <p style='text-align:center;font-size:60px;font-weight:bold; color:red'>Descrição de Alimentos em Imagens</p>
