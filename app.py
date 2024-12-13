4bde2ad2475be011a4ee9fe7afa68ce3fbd5a2f5
Silvio Lima <silviolima07@gmail.com>
2024-12-13 13:47:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,260 @@ completion = client_groq.chat.completions.create(
     stop=None,
 )
 
-st.write(completion.choices[0].message)
+print(completion.choices[0].message)
 
-   
+
+client = Groq()
+
+st.write("Client groq:", client)
+"""
+# Criando o checkbox para mostrar ou não os comandos das tasks
+#mostrar_comandos = st.checkbox("Mostrar progresso das tarefas em execução", value=True)
+
+# Função para mostrar o progresso da execução das tarefas e capturar o resultado final
+#def executar_tarefas(crew, inputs):
+#    st.write("### Executando as tasks...")
+
+    # Variável para armazenar o resultado final após a execução de todas as tasks
+#    result = None
+
+    # Executa as tasks uma por uma e exibe o progresso no Streamlit
+#    for i, task in enumerate(crew.tasks):
+#        task_agent = (task.agent.role) # Nome do agente responsavel, definido em my_agents
+#        task_name = (task.name).upper()  # Nome da tasks, definido tem my_tasks
+#        st.write(f"Agent : **{task_agent}**")  # Mostra o nome do agent
+#        st.write(f"Executando task : **{task_name}**")  # Mostra o nome da task
+#        st.write(f"Descrição:")
+#        st.write(f"{task.description}")
+#        time.sleep(2)  # Simula o tempo de execução da task
+        # Aqui você pode simular o progresso de cada task, ou capturar a execução real
+
+    # Após a execução de todas as tasks, salva o resultado
+#    result = crew.kickoff(inputs=inputs)
+    
+#    return result  # Retorna o resultado final
+
+
+# Função para ler o PDF e extrair o texto
+#def extract_text_from_pdf(uploaded_file):
+#    text_content = ""
+#    with pdfplumber.open(uploaded_file) as pdf:
+#        for page in pdf.pages:
+#            text_content += page.extract_text() + "\n"
+#    return text_content
+
+# Função para salvar o conteúdo extraído em um arquivo txt
+#def save_to_txt(text_content, output_filename="profile.txt"):
+#    with open(output_filename, "w", encoding="latin1") as text_file:
+#        text_file.write(text_content)
+
+# Função para ler o conteúdo de um arquivo markdown
+#def read_markdown_file(file_path):
+#    if os.path.exists(file_path):
+#        with open(file_path, "r", encoding="utf-8") as file:
+#            return file.read()
+#    else:
+#        raise FileNotFoundError(f"Arquivo {file_path} não encontrado.")
+
+#def executar_task(analise, image):
+#     mensagem = {
+#         "role": "user",
+#         "content": f"Analise a seguinte imagem: {image}"
+#     }
+#    
+#     try:
+#         resultado = analise.invoke(mensagem)
+#         return resultado
+#     except Exception as e:
+#         st.error(f"Ocorreu um erro: {e}")
+
+
+
+# Função para redimensionar a imagem e convertê-la para Base64
+def resize_image(image_path, max_width=200):
+    img = Image.open(image_path)
+    st.write("Image process...")
+    
+    # Redimensiona a imagem mantendo a proporção
+    #st.write('Width:',img.width, 'Max:',200)
+    if img.width > max_width:
+        
+        ratio = max_width / img.width
+        new_size = (max_width, int(img.height * ratio))
+        img = img.resize(new_size, Image.Resampling.LANCZOS)
+        st.write("- resized")
+        
+
+    # Converte a imagem redimensionada para Base64
+    #buffer = io.BytesIO()
+    #img.save(buffer, format="PNG")
+    #img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    image_path="image.png"
+    img.save(image_path)
+    img_base64 = encode_image(image_path)
+    st.write("- converted to base64")
+    st.image(img)
+    
+    return img_base64
+    
+llama = llama_groq
+llava = llava_groq
+    
+
+# Configuração da crew com o agente recrutador
+agente_nutri = criar_agente(llama)
+#st.write("Objetivo: "+agente_nutri.goal)
+# Cria a task usando o agente criado
+#st.write('Criar a task')
+task_analise = criar_task(agente_nutri)
+
+
+html_page_title = """
+     <div style="background-color:black;padding=60px">
+         <p style='text-align:center;font-size:60px;font-weight:bold; color:red'>Descrição de Alimentos em Imagens</p>
+     </div>
+               """               
+st.markdown(html_page_title, unsafe_allow_html=True)
+
+garfo_colher = Image.open("img/garfo_colher2.png")
+st.sidebar.image(garfo_colher,caption="",use_column_width=True)
+
+st.sidebar.markdown("# Menu")
+option = st.sidebar.selectbox("Menu", ["Image", 'About'], label_visibility='hidden')
+
+if option == 'Image':
+    st.markdown("#### Objetivo: informar se é saudável ou não.")
+    
+    try:
+        st.markdown("## Upload Image")
+        uploaded_img = st.file_uploader("Envie imagem em PNG/JPG", type=['png', 'jpeg'])
+        if uploaded_img is not None:
+            img = Image.open(uploaded_img) # Load the image
+            image_path="image.png"
+            img.save(image_path)
+            # Getting the base64 string
+            base64_image = encode_image(image_path)
+            # Usando HTML para centralizar a imagem
+            st.markdown(
+        f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="data:image/png;base64,{base64_image}" alt="Imagem" style="width: 80%; height: auto;">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+            
+            prompt = dedent("""
+            
+    You are an expert assistant in recognizing and describing foods in images with precision.
+    
+    Your role is to analyze images and provide description of foods, giving details, like color,size and accurate descriptions of foods.
+
+    Always considering only foods present in the image.
+    
+    Never describe cars, trucks,  places in image, focus on foods only.
+    
+    If the image contains no food, respond only with the phrase: 'None food in image.'
+    """)
+            
+            # Configuração da crew com o agente recrutador
+            #agente_nutri = criar_agente(llama)
+            #st.write("Objetivo: "+agente_nutri.goal)
+            # Cria a task usando o agente criado
+            #st.write('Criar a task')
+            #task_analise = criar_task(agente_nutri)
+            #st.write(task_analise)
+            st.write(" ")
+            st.markdown("## Analisar Imagem")
+            idioma = selecionar_idioma()
+            
+            #st.info("#### Avalie sempre a resposta final. O agente tem razão ou não?")
+            
+            
+            crew = Crew(
+                  agents=[agente_nutri],
+                  tasks=[task_analise],
+                  process=Process.sequential,  # Processamento sequencial das tarefas
+                  verbose=True
+                 
+               )
+            #st.write(crew)
+            #st.markdown("#### "+prompt)
+
+            if st.button("INICIAR"):
+                #inputs = {
+                #      'question': "Fornecer uma descrição detalhada da imagem",
+                #      'image': base64_image,
+                #      'descricao': 'descricao_img.md'}
+                      
+                with st.spinner('Wait for it...we are working...please'):
+                    # Executa o CrewAI
+                    
+                    
+                    try:
+                    
+                            
+                        descricao = image_to_text(client, llava, base64_image, prompt)
+                        # Exibindo a descricao
+                        #st.write("Descrição da imagem:")
+                        
+                        #st.write(descricao)
+                        
+                        inputs = {
+                      'descricao': descricao,
+                      'idioma': idioma}
+            
+                        #result = crew.kickoff(inputs=inputs)
+                        #st.write(result)
+                        #executar_tarefas(crew)
+                        #executar_task(task_analise, base64_image)
+                        
+                        # Criando a task
+                        #task = criar_task(agent)
+
+                        # Configurando a crew com a task
+                        #crew = configurar_crew(agente1, task_analise, base64_image)
+
+                        # Executando a crew
+                        answer = "None food in image"
+                        if descricao.lower() != answer.lower():
+                            
+                            resultado = executar_crew(crew, inputs)
+                            result_text = resultado.raw
+                            #st.write(resultado)
+                            #st.write(result_text)
+                            # Exibindo o texto com um tamanho de fonte maior
+                            # Substituindo quebras de linha por <br> e aplicando o estilo a todo o texto
+                            st.markdown(f"<div style='font-size:23px'>{result_text.replace(chr(10), '<br>')}</div>", unsafe_allow_html=True)
+                        else:
+                            st.markdown("### "+descricao)
+                        
+                    except Exception as e:
+                        st.error(f"Ocorreu um erro ao executar a crew: {e}")
+        
+        else:
+            st.markdown("##### Formato PNG/JPEG")        
+    except:
+        st.error("Houston, we have a problem.")
+if option == 'About':
+    prato = Image.open("img/prato.png")
+    st.sidebar.image(prato,caption="",use_column_width=True)
+    st.markdown("### A partir da descrição da imagem feita por um agente especialista, usando o modelo llava,  um agente nutricionista, com modelo llama, informa se alimento é saudável.")
+    st.markdown("### Modelos acessados via Groq.")
+    st.markdown("### Exemplo:")
+    st.write("""
+    Ao analisar os alimentos descritos na imagem, podemos identificar os seguintes alimentos saudáveis:
+
+* Salada com alface, tomate e cenoura: a alface é rica em fibras e vitamina A, enquanto os tomates fornecem vitamina C e lycopene, e as cenouras são ricas em vitamina A e fibras.
+* Tomates: ricos em vitamina C e lycopene, um antioxidante que ajuda a prevenir doenças crônicas.
+* Cenouras: são ricas em vitamina A e fibras, importante para a saúde dos olhos e do sistema imunológico.
+
+Já os alimentos que podem ser considerados menos saudáveis ou que devem ser consumidos em moderação são:
+
+* Carne (beef e sausage): embora sejam boas fontes de proteínas, a carne vermelha consumida em excesso pode contribuir para o aumento do risco de doenças cardíacas e câncer. Além disso, a gordura presente na carne pode ser alta em calorias e gorduras saturadas. O consumo exagerado de sausage também pode levar ao consumo excessivo de sódio e preservantes.
+* Arroz: embora seja uma boa fonte de carboidratos, o arroz branco refinado pode ser pobre em fibras e nutrientes essenciais. É importante optar por opções mais saudáveis, como arroz integral.
+Em resumo, a imagem apresenta uma refeição equilibrada, com uma balança adequada entre proteínas, 
+carboidratos e vegetais. No entanto, é importante ter cuidado com a quantidade de carne e arroz 
+consumidos e optar por opções mais saudáveis. Além disso, a adição de salsa ao prato pode ser benéfica, 
+pois fornece vitamina C e flavonoides..""")    
    
