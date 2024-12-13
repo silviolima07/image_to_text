64c639cb201dc2fa5788e7bccc229459e1adfd79
Silvio Lima <silviolima07@gmail.com>
2024-12-13 14:00:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -152,11 +152,8 @@ load_dotenv()
 
 # Obter a chave da API GROQ
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-st.write("Groq key:", GROQ_API_KEY)
 
 client = Groq()
-
-st.write("Client groq:", client)
 
 # Criando o checkbox para mostrar ou não os comandos das tasks
 #mostrar_comandos = st.checkbox("Mostrar progresso das tarefas em execução", value=True)
