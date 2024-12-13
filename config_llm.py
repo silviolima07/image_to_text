b952f62a8232bee508923f21cf11e580352b2a0f
Silvio Lima <silviolima07@gmail.com>
2024-12-13 16:41:56 -0300

    Update config_llm.py

--- config_llm.py
+++ config_llm.py
@@ -9,7 +9,7 @@ class MyLLM():
     GPT_o1    = LLM(model='01-preview')
     GPT3_5    = LLM(model='gpt-3.5-turbo')
     LLAMA3_70B    = LLM(model='llama3-70b-8192')
-    GROQ_LLAMA    = LLM(model='llama-3.2-11b-vision-preview')
+    GROQ_LLAMA    = LLM(model='groq/llama-3.2-11b-vision-preview')
     GROQ_MIXTRAL    = LLM(model='groq/mixtral-8x7b-32768')
     GROQ_LLAMA2    = LLM(model='groq/llama-3.2-11b-vision-preview')
       
